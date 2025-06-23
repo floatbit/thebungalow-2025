07ef0fcfae066fb2bd85107358125e8f719725f6
“Chris <chris@floatbit.com>
2025-06-22 22:19:48 -0400

    blocks

--- footer.php
+++ footer.php
@@ -5,6 +5,8 @@
         <?php $lines = [
             'line1' => get_template_directory() . '/dist/images/line1.svg',
             'line2' => get_template_directory() . '/dist/images/line2.svg',
+            'line3' => get_template_directory() . '/dist/images/line3.svg',
+            'line4' => get_template_directory() . '/dist/images/line4.svg',
         ]; ?>
         <?php foreach ($lines as $index=>$line) : ?>
             <div class="line-item line-<?php echo $index; ?>">
