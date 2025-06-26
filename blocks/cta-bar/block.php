d526f9395a4b39dbd6f40a1a670fa2f7d0e7c21e
“Chris <chris@floatbit.com>
2025-06-26 18:08:07 -0400

    styles

--- blocks/cta-bar/block.php
+++ blocks/cta-bar/block.php
@@ -30,10 +30,12 @@ $classes .= ' ' . get_field('bottom_margin');
 ?>
 
 <div id="<?php echo esc_attr( $id ); ?>" class="<?php echo esc_attr( $classes ); ?>">
-    <div class="block bg-secondary text-white">
-        <div class="flex items-center justify-center gap-8 py-4">
-            <p class="text-secondary-dark uppercase h4 md:mb-0">Wanna stay in the loop?</p>
-            <a class="btn bg-secondary-dark inline-block px-4 py-2 uppercase h5" href="#">Subscribe</a>
+    <div class="cta-bar">
+        <div class="block bg-secondary text-white">
+            <div class="flex items-center justify-center gap-8 py-4">
+                <p class="text-secondary-dark uppercase h4 md:mb-0">Wanna stay in the loop?</p>
+                <a class="btn bg-secondary-dark inline-block px-4 py-2 uppercase h5" href="#">Subscribe</a>
+            </div>
         </div>
     </div>
 </div>
--- parts/section-header.php
+++ parts/section-header.php
@@ -1,14 +1,15 @@
 <?php
     $logo = get_location_logo();
 ?>
-<header>
+<header class="sticky top-0 z-50 bg-white">
     <div class="container">
-        <div class="flex items-center justify-between">
+        <div class="flex items-center justify-between relative">
             <div class="logo">
                 <a href="/">
                     <img src="<?php echo $logo['url']; ?>" alt="Logo">
                 </a>
             </div>
+            <a class="mobile-toggle btn inline-block md:hidden" href="#">Menu</a>
         </div>
     </div>
 </header>
