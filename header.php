378f9cf2248e27aef2ca7939c962667ca0cdb3c4
“Chris <chris@floatbit.com>
2025-09-24 12:10:13 -0400

    favicon

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
+    <link rel="icon" href="<?php echo assets_url('/dist/images/favicon.png'); ?>" />
 </head>
 
 <body <?php body_class('antialiased'); ?>>
