0d08b2168294835b1134fc23d33a8396526607f0
“Chris <chris@floatbit.com>
2026-05-01 17:12:36 -0400

    popup

--- parts/popup.php
+++ parts/popup.php
@@ -6,7 +6,9 @@ $post = get_post();
 <?php if ($location_fields['show_popup'] && $location_fields['popup_image']): ?>
 <div class="popup">
     <div class="image-container">
-        <img src="<?php echo $location_fields['popup_image']['url']; ?>" alt="<?php echo esc_attr($location_fields['popup_image']['alt']); ?>" class="w-full"/>
+        <a href="<?php echo $location_fields['popup_url']; ?>" target="_blank">
+            <img src="<?php echo $location_fields['popup_image']['url']; ?>" alt="<?php echo esc_attr($location_fields['popup_image']['alt']); ?>" class="w-full"/>
+        </a>
     </div>
 </div>
 <?php endif; ?>
